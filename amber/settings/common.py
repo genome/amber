b24218ceec1c7d227cf4b10ea29167f577d947b4
David Morton <davidlmorton@gmail.com>
2014-02-20 14:32:07 -0600

    Add different settings for development and production

--- amber/settings/common.py
+++ amber/settings/common.py
@@ -8,38 +8,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pg31=tl)*^-m)o8qqxayk7m(8i#)#b#l2*ir!hsywo)ze2ta*g'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
+DEFAULT_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = [
     'tastypie',
-    'persistence',
     'json_field',
-)
+]
+
+LOCAL_APPS = [
+    'persistence',
+]
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -50,32 +42,16 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'amber.urls'
-
 WSGI_APPLICATION = 'amber.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -83,3 +59,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+ROOT_URLCONF = 'amber.urls'
---
+++ amber/settings/production.py
@@ -0,0 +1,20 @@
+from .common import *
+
+import os
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['AMBER_SECRET_KEY']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['AMBER_DB_NAME'],
+        'USER': os.environ['AMBER_DB_USER'],
+        'PASS': os.environ.get('AMBER_DB_PASS'),
+        'HOST': os.environ.get('AMBER_DB_HOST', ''),
+        'PORT': os.environ.get('AMBER_DB_PORT', '5432'),
+    },
+}
